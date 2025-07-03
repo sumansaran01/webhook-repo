633cfa74de9543319793a874c98eab5f8e0ab288
Suman <146095753+sumansaran01@users.noreply.github.com>
2025-07-03 09:38:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.post("/webhook", (req, res) => {
   res.sendStatus(200);
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Webhook server running on port ${PORT}`);
 });
